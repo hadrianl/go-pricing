8c883776a06c549f2e4291aa86ec79e2565038cf
hadrianl <137150224@qq.com>
2019-11-11 17:08:50 +0800

    debug

--- option.go
+++ option.go
@@ -3,10 +3,10 @@ package option
 import (
 	"fmt"
 
-	"github.com/hadrianl/go-pricing/base/bs"
+	blackscholes "github.com/hadrianl/go-pricing/base/black-scholes"
 )
 
 func Calc() {
 	// price := bs.CalcPrice(100, 95, 0.02, 3/12, 0.5, 1)
-	fmt.Println(bs.Measure(90, 100, 0.1, 1.0/12, 0.35, 0.05, 1))
+	fmt.Println(blackscholes.Measure(90, 100, 0.1, 1.0/12, 0.35, 0.05, 1))
 }
--- option_test.go
+++ option_test.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hadrianl/go-pricing/base/bs"
+	blackscholes "github.com/hadrianl/go-pricing/base/black-scholes"
 )
 
 func TestCalc(t *testing.T) {
 
-	fmt.Println(bs.Measure(90.0, 100, 0.1, 3.0/12, 0.35, 0.05, -1))
+	fmt.Println(blackscholes.Measure(90.0, 100, 0.1, 3.0/12, 0.35, 0.05, -1))
 }
 
 func BenchmarkCalc(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		bs.Measure(90.0, 100, 0.1, 3.0/12, 0.35, 0.05, -1)
+		blackscholes.Measure(90.0, 100, 0.1, 3.0/12, 0.35, 0.05, -1)
 	}
 }
